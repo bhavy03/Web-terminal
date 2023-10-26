0d61cc17a4ebac1c0ea52f86beed70fee449b1d4
Bhavya <bhavya7664@gmail.com>
2023-10-26 09:49:10 +0530

    initail commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+// import Apps from './components/Help.jsx'
+import Terminal from './components/Terminal.jsx'
+
+function App() {
+
+  return (
+    <Terminal />
+    // <Apps />
+  )
+}
+
+export default App
